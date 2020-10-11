a968737a3f4c947b7a84b63569a1f5fe4637815a
Ahmad <ahmadshafizadeh93@gmail.com>
2020-10-11 22:58:45 +0330

    Add custom recycler adapter for header decoration

---
+++ headerlibrary/src/main/java/ir/teamtea/headerlibrary/recycler/HeaderAdapter.java
@@ -0,0 +1,46 @@
+package ir.teamtea.headerlibrary.recycler;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public abstract class HeaderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public abstract List getRecyclerItems();
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void onViewAttachedToWindow(@NonNull RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        if (holder instanceof CustomHeaderViewHolder) {
+            ((CustomHeaderViewHolder) holder).onAttach();
+        }
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(@NonNull RecyclerView.ViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+        if (holder instanceof CustomHeaderViewHolder) {
+            ((CustomHeaderViewHolder) holder).onDetach();
+        }
+    }
+
+}
