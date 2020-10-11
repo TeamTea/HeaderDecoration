9b50a6ffc43bb2fa3a288c5a0a808af886a0c5f1
Ahmad <ahmadshafizadeh93@gmail.com>
2020-10-11 22:57:39 +0330

    Add custom recycler viewHolder for Header decoration

---
+++ headerlibrary/src/main/java/ir/teamtea/headerlibrary/recycler/CustomHeaderViewHolder.java
@@ -0,0 +1,21 @@
+package ir.teamtea.headerlibrary.recycler;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class CustomHeaderViewHolder<T> extends RecyclerView.ViewHolder {
+
+    public CustomHeaderViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void onDetach() {
+
+    }
+
+    public void onAttach() {
+
+    }
+}
